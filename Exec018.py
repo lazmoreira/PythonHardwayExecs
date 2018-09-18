ef60856aa02b860497fe9f47fbbaaf10fa6da768
Lazaro Moreira <lazaro_rk@hotmail.com>
2018-09-18 16:36:18 +0300

    Exec018 - Fixes

--- Exec018.py
+++ Exec018.py
@@ -3,13 +3,16 @@ def print_two(*args):
     arg1, arg2 = args
     print(f"arg1: {arg1}, arg2: {arg2}")
 
+
 # ok, that *args is pointless, we can just do this
 def print_two_again(arg1, arg2):
     print(f"arg1: {arg1}, arg2: {arg2}")
 
+
 # this just take one argument
 def print_one(arg1):
     print(f"arg1: {arg1}")
+
 
 # this one takes no argument
 def print_none():
